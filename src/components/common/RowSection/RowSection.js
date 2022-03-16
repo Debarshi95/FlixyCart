4937cef5d6188a7735e24378526207cfc24adbac
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-03-16 22:56:02 +0530

    Add components RowSection, BookCard

---
+++ src/components/common/RowSection/RowSection.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import BookCard from '../BookCard/BookCard';
+import Typography from '../Typography/Typography';
+import './RowSection.scss';
+
+const RowSection = ({ title, titleAlign, books, ...props }) => {
+  return (
+    <section className="flex-1 RowSection__root" {...props}>
+      <Typography variant="h4" align={titleAlign} className="RowSection__title">
+        {title}
+      </Typography>
+      <div className="RowSection__row">
+        {books?.map((book) => (
+          <BookCard key={book._id} book={book} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default RowSection;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as RowSection } from './common/RowSection/RowSection';
+export { default as Navbar } from './common/Navbar/Navbar';
+export { default as BookCard } from './common/BookCard/BookCard';
+export { default as BannerCard } from './home/BannerCard/BannerCard';
+export { default as Banner } from './home/Banner/Banner';
+export { default as CardImage } from './common/CardImage/CardImage';
+export { default as Footer } from './common/Footer/Footer';
