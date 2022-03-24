88f316a6bd67b87eb968e80de4119baa5923d63b
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-03-24 23:15:03 +0530

    Update components Button Footer

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,39 +1,33 @@
 import React from 'react';
+import cn from 'classnames';
 import { Link } from 'react-router-dom';
 import './Button.scss';
 
-const Button = ({
-  text,
-  loading,
-  loadingText,
-  variant,
-  className,
-  children,
-  to,
-  link,
-  ...props
-}) => {
-  const TextComp = <p className="Button__text">{loading ? loadingText : children}</p>;
+const buttonComponents = {
+  link: Link,
+  button: 'button',
+  div: 'div',
+};
+const Button = ({ component, text, variant, className, children, ...props }) => {
+  const Component = buttonComponents[component];
 
   return (
-    <div role="button" {...props} className={`Button__root Button__${variant} ${className}`}>
-      {link ? (
-        <Link to={to} className="Button__linkText">
-          {children}
-        </Link>
-      ) : (
-        TextComp
-      )}
-    </div>
+    <Component
+      className={cn('Button__root', className, {
+        [`Button--${variant}`]: true,
+      })}
+      {...props}
+    >
+      {children}
+    </Component>
   );
 };
 
 Button.defaultProps = {
-  loadingText: 'Loading...',
-  loading: false,
   children: '',
   variant: 'contained',
-  link: false,
   to: '/',
+  className: '',
+  component: 'link',
 };
 export default Button;
--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { FaHeart } from 'react-icons/fa';
+import Typography from '../Typography/Typography';
 import './Footer.scss';
 
 const Footer = () => {
   return (
     <div className="Footer__root">
-      Made with <FaHeart color="red" /> by Debarshi
+      <Typography variant="h6">
+        {' '}
+        Made with <FaHeart /> by Debarshi
+      </Typography>
     </div>
   );
 };
