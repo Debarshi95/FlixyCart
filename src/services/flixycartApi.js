d70796cb8bff3a25faf6bcfcc49c626f77ee42b7
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-03-26 00:05:27 +0530

    Update services, add validation schema

--- src/services/flixycartApi.js
+++ src/services/flixycartApi.js
@@ -1,6 +1,8 @@
 import generateApiClient from '../utils/generateClient';
 
 const flixyCartApi = generateApiClient(process.env.REACT_APP_BASE_URL);
-const getProducts = () => flixyCartApi.get(`/books`);
 
-export { getProducts };
+const getProducts = () => flixyCartApi.get(`/books`);
+const signIn = (data) => flixyCartApi.post('/auth/login', { ...data });
+
+export { getProducts, signIn };
