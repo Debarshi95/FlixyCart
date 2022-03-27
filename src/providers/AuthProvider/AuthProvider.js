db6b94f04296a892781e329a585a2acd74ef4bf6
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-03-27 21:48:32 +0530

    Update AuthProvider

--- src/providers/AuthProvider/AuthProvider.js
+++ src/providers/AuthProvider/AuthProvider.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import { getItem } from '../../utils/helperFuncs';
 
-const AuthContext = createContext({ user: null });
+const AuthContext = createContext({ user: null, setUser: () => {} });
 
 export const useAuth = () => useContext(AuthContext);
 
@@ -9,15 +9,16 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const persistedData = getItem('user');
-    if (persistedData) {
-      setUser(persistedData);
+    const persistedUser = getItem('user');
+
+    if (persistedUser) {
+      setUser(persistedUser);
     } else {
       setUser(null);
     }
   }, []);
 
-  const value = useMemo(() => ({ user }), [user]);
+  const value = useMemo(() => ({ user, setUser }), [user]);
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
