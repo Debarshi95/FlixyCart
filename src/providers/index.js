41a5b7bd46b143ffc7d08496af1c750099f61354
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-03-30 00:27:11 +0530

    Update actions, App, index styles etc

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,2 +1,4 @@
 export { default as ThemeProvider } from './ThemeProvider/ThemeProvider';
 export { default as AuthProvider } from './AuthProvider/AuthProvider';
+export { default as FilterProvider } from './FilterProvider/FilterProvider';
+export { default as ProductProvider } from './ProductProvider/ProductProvider';
