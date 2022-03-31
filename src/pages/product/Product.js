d5b34148a8c7d0c71605ab22567dc1327a380e69
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-03-31 22:11:42 +0530

    Update Home, Product

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useProducts } from '../../providers/ProductProvider/ProductProvider';
+import { useFilter } from '../../providers/FilterProvider/FilterProvider';
+import { BookCard, Sidebar } from '../../components';
+import './Product.scss';
 
 const Product = () => {
-  return <div>Product</div>;
+  const {
+    productState: { products = [] },
+  } = useProducts();
+  const { filteredState, getSortedProducts, getFilteredProducts } = useFilter();
+
+  console.log({ filteredState });
+  const sortedProducts = getSortedProducts(products, filteredState.sortBy);
+
+  const filteredProducts = getFilteredProducts(sortedProducts, filteredState);
+
+  console.log({ filteredProducts, sortedProducts });
+  return (
+    <div className="Product__root w-full d-flex">
+      <Sidebar />
+      <section className="Product__itemContainer w-full">
+        {filteredProducts.map((book) => (
+          <BookCard book={book} key={book.id} />
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default Product;
