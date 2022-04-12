221fd2e71de898d6a22292a692b3918add118420
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-04-13 00:34:31 +0530

    Remove card link, update styles

--- src/components/common/BookCard/BookCard.js
+++ src/components/common/BookCard/BookCard.js
@@ -1,6 +1,5 @@
 import React, { memo } from 'react';
 import cn from 'classnames';
-import { Link } from 'react-router-dom';
 import Button from 'components/common/Button/Button';
 import CardImage from 'components/common/CardImage/CardImage';
 import Typography from 'components/common/Typography/Typography';
@@ -20,58 +19,56 @@ const BookCard = ({
   ...props
 }) => {
   return (
-    <Link to={`/book/${book._id}`}>
-      <div
-        className={cn('BookCard__root d-flex', className, {
-          [`flex-${orientation}`]: true,
-          [`h-${cardHeight}`]: true,
-          [`w-${cardWidth}`]: true,
-        })}
-        {...props}
-      >
-        <CardImage url={imageUrl || book.image} {...imageProps} />
-        <div className={cn('BookCard__content', { contentClassName })}>
-          {children || (
-            <>
-              <div>
-                <Typography variant="h6" className="Typography--ellipsis" size="sm">
-                  {book.title}
-                </Typography>
-                <div className="d-flex">
-                  {book.categories.map((item, idx) => (
-                    <p key={idx} className="Typography--primary Typography--xs">
-                      {item}{' '}
-                    </p>
-                  ))}
-                </div>
-                <div className="d-flex content-between">
-                  <Typography
-                    variant="p"
-                    className="Typography--ellipsis max-w-60"
-                    textbold
-                    size="xs"
-                  >
-                    {book.author}
-                  </Typography>
-                  <Typography variant="p" textbold size="xs">
-                    Rs {book.price}
-                  </Typography>
-                </div>
+    <div
+      className={cn('BookCard__root d-flex', className, {
+        [`flex-${orientation}`]: true,
+        [`h-${cardHeight}`]: true,
+        [`w-${cardWidth}`]: true,
+      })}
+      {...props}
+    >
+      <CardImage url={imageUrl || book.image} {...imageProps} />
+      <div className={cn('BookCard__content', { contentClassName })}>
+        {children || (
+          <>
+            <div>
+              <Typography variant="h6" className="Typography--ellipsis" size="sm">
+                {book.title}
+              </Typography>
+              <div className="d-flex">
+                {book.categories.map((item, idx) => (
+                  <p key={idx} className="Typography--primary Typography--xs">
+                    {item}{' '}
+                  </p>
+                ))}
               </div>
+              <div className="d-flex content-between">
+                <Typography
+                  variant="p"
+                  className="Typography--ellipsis max-w-60"
+                  textbold
+                  size="xs"
+                >
+                  {book.author}
+                </Typography>
+                <Typography variant="p" textbold size="xs">
+                  Rs {book.price}
+                </Typography>
+              </div>
+            </div>
 
-              <Button
-                variant="contained"
-                component="button"
-                className="BookCard__button  text-bold w-full"
-                {...buttonProps}
-              >
-                Add To Cart
-              </Button>
-            </>
-          )}
-        </div>
+            <Button
+              variant="contained"
+              component="button"
+              className="BookCard__button  text-bold w-full"
+              {...buttonProps}
+            >
+              Add To Cart
+            </Button>
+          </>
+        )}
       </div>
-    </Link>
+    </div>
   );
 };
 BookCard.defaultProps = {
